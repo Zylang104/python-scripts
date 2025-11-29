8fe93a07269dd1f338da25638f2efbcd0aeeb13f
Zylang104 <joninurka7@gmail.com>
2025-11-29 16:11:47 +0100

    Create loweryourtoneplease.py

---
+++ very_useless/loweryourtoneplease.py
@@ -0,0 +1,5 @@
+print("say something and i will lower your character to make it lower")
+while True:
+    text = input("enter your text: ")
+    lowered_text = text.lower()
+    print("here is your lowered text:" + lowered_text)
