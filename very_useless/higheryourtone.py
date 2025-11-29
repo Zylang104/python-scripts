6cfcb48d92b9b6a314807244a650bfb3d696fdb6
Zylang104 <joninurka7@gmail.com>
2025-11-29 20:13:25 +0100

    Update higheryourtone.py

---
+++ very_useless/higheryourtone.py
@@ -0,0 +1,5 @@
+print("say something and i will lower your character to make it lower")
+while True:
+    text = input("enter your text: ")
+    higher_text = text.upper()
+    print("here is your higher text:" + higher_text)
